b55869365677398ebc103c1755faa6f859868222
Simon-DaSilva <simon.dasilva.to@gmail.com>
2024-03-28 11:26:19 -0400

    added music that plays when instrument is dragged

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
+const bass = document.querySelector("#bass");
+const clarinet = document.querySelector("#clarinet");
+const piano = document.querySelector("#piano");
+const trumpet = document.querySelector("#trumpet");
+const saxaphone = document.querySelector("#saxaphone");
+const drum = document.querySelector("#drum");
+const trombone = document.querySelector("#trombone");
+
 let drag = document.querySelectorAll('.icons img'),
 dropZones = document.querySelectorAll('.drop-zone'),
 draggedPiece;
+
+function refreshPage(){
+    window.location.reload();
+} 
 
 function handleStartDrag() { 
 	console.log('started dragging this piece:', this);
@@ -16,8 +28,18 @@ function handleDrop(e) {
 	e.preventDefault();
 	console.log('dropped something on me');
 	this.appendChild(draggedPiece);
+    playAudio(draggedPiece.id, this)
 }
 
+function playAudio(selectedInstrument, selectedDropzone) {
+    console.log(selectedInstrument);
+    let instrument = document.createElement("audio");    
+        instrument.src = `audio/${selectedInstrument}.mp3`;
+        instrument.load();
+        selectedDropzone.appendChild(instrument); 
+        instrument.loop = true;
+        instrument.play();
+}
 
 drag.forEach(part => part.addEventListener("dragstart", handleStartDrag));
 dropZones.forEach(zone => zone.addEventListener("dragover", handleDragOver));
